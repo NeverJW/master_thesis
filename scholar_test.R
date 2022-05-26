10d888b436afa5ab0cfa0cdae5df8ce56274fd65
Jingwen <jingwen.xu@student.uni-tuebingen.de>
2022-05-26 22:55:39 +0200

    setup script

---
+++ scholar_test.R
@@ -0,0 +1,8 @@
+library(scholar)
+id <- 'GAi23ssAAAAJ'
+id <- 'cjNOgrcAAAAJ'
+predict_h_index(id)
+get_citation_history(id)
+get_profile(id)
+get_publications(id)
+get_num_articles(id) 
---
+++ setup.R
@@ -0,0 +1,13 @@
+#### Loading libraries needed ####
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("xml2")) install.packages("xml2")
+if (!require("XML")) install.packages("XML")
+if (!require("RSelenium")) install.packages("RSelenium")
+if (!require("readxl")) install.packages("readxl")
+
+
+library(tidyverse)
+library(xml2)
+library(XML)
+library(RSelenium)
+library(readxl)
