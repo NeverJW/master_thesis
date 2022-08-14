adf7efe699b7dc123a8a90f3c4f049388c229b9f
Jingwen <jingwen.xu@student.uni-tuebingen.de>
2022-08-14 15:37:03 +0200

    test scholar package to consider the columns need to be kept

--- scholar_test.R
+++ scholar_test.R
@@ -1,8 +1,39 @@
-library(scholar)
-id <- 'GAi23ssAAAAJ'
-id <- 'cjNOgrcAAAAJ'
-predict_h_index(id)
-get_citation_history(id)
-get_profile(id)
-get_publications(id)
+###################### test scholar package ################################
+# to see which information we need to keep in the database
+
+# Define the id for Richard Feynman
+id <- 'B7vSqZsAAAAJ'
+
+# Get his profile and print his name
+l <- get_profile(id)
+l$h_index
+l$total_cites  # these two columns gonna be keeped 
+
 get_num_articles(id) 
+
+get_num_top_journals(id)
+
+get_oldest_article(id)
+
+get_num_distinct_journals(id)
+
+# Get his citation history, i.e. citations to his work in a given year 
+get_citation_history(id)
+
+# Get his publications (a large data frame)
+get_publications(id)
+
+## Predict h-index of original method author
+predict_h_index(id)
+
+#### compare the citations of different scholars 
+# Compare Feynman and Stephen Hawking
+ids <- c('B7vSqZsAAAAJ', 'qj74uXkAAAAJ')
+
+# Get a data frame comparing the number of citations to their work in
+# a given year 
+compare_scholars(ids)
+
+# Compare their career trajectories, based on year of first citation
+compare_scholar_careers(ids)
+
