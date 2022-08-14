b1f0adcb1b56aabce1b3b2a86c5055548332a817
Jingwen <jingwen.xu@student.uni-tuebingen.de>
2022-08-14 16:04:51 +0200

    update scholar test comments

--- scholar_test.R
+++ scholar_test.R
@@ -7,24 +7,24 @@ id <- 'B7vSqZsAAAAJ'
 # Get his profile and print his name
 l <- get_profile(id)
 l$h_index
-l$total_cites  # these two columns gonna be keeped 
+l$total_cites  # these two variables gonna be keeped 
 
 get_num_articles(id) 
 
-get_num_top_journals(id)
+get_num_top_journals(id) 
 
 get_oldest_article(id)
 
 get_num_distinct_journals(id)
 
 # Get his citation history, i.e. citations to his work in a given year 
-get_citation_history(id)
+get_citation_history(id)   # from here get the mean of citations each year for certain author
 
 # Get his publications (a large data frame)
 get_publications(id)
 
 ## Predict h-index of original method author
-predict_h_index(id)
+predict_h_index(id)   # because this is designed for neuroscientist, it doesn't be included 
 
 #### compare the citations of different scholars 
 # Compare Feynman and Stephen Hawking
